86e17ee7a903f3f6aeed1e00ca286ca1b3c553a3
rpat9 <rudrap26176@gmail.com>
2025-05-13 19:33:06 -0500

    Updated ingredientList mobile UI

--- src/components/ingredient_list.jsx
+++ src/components/ingredient_list.jsx
@@ -4,7 +4,7 @@ import IngredientProgress from "./ingredient_progress.jsx"
 export default function IngredientList({ ingredientsArray, getRecipe, onDeleteIngredient }){
 
     const ingredientsList = ingredientsArray.map(ingredient => (
-        <li className="bg-[var(--color-primary)] text-[var(--button-text-color)] text-lg font-semibold w-full mb-3 p-2 flex justify-between hover:scale-102 hover:bg-[var(--button-hover-primary)] transition duration-300 ease-in-out cursor-default" key = {ingredient}> 
+        <li className="bg-[var(--color-primary)] text-[var(--button-text-color)] text-base md:text-lg font-semibold w-full mb-3 p-2 flex justify-between hover:scale-102 hover:bg-[var(--button-hover-primary)] transition duration-300 ease-in-out cursor-default" key = {ingredient}> 
             {ingredient}
 
             <button className="cursor-pointer" 
@@ -27,22 +27,22 @@ export default function IngredientList({ ingredientsArray, getRecipe, onDeleteIn
 
             <div className="flex justify-center flex-col items-center">
 
-                <h2 className="block !font-bold text-[var(--color-primary)] text-[1.5em] mt-[0.83em] mb-[0.2em]">Ingredients list: </h2>
+                <h2 className="block !font-bold text-[var(--color-primary)] text-lg md:text-[1.5em] mt-[0.83em] mb-[0.2em]">Ingredients list: </h2>
 
-                <ul className="mt-2 w-full min-w-3xs max-w-3xl">
+                <ul className="mt-2 w-full min-w-xs md:min-w-3xs max-w-3xl">
                     {ingredientsList}
                 </ul>
 
             </div>
 
-            {ingredientsArray.length >= 5 ? <div className="flex justify-between items-center mt-4 card border border-[var(--color-secondary)] rounded-xl min-w-3xs max-w-3xl p-2 mx-auto w-full">
+            {ingredientsArray.length >= 5 ? <div className="flex flex-col md:flex-row justify-between items-center mt-4 card border border-[var(--color-secondary)] rounded-xl min-w-xs md:min-w-3xs max-w-3xl p-2 mx-auto w-full">
 
-                <div className="pr-5 pl-5 pb-2">
-                    <h3 className="block !font-bold text-[1.17em] mt-[0.83em] mb-[0.1em] text-[var(--color-primary)]">Ready for a recipe?</h3>
-                    <p classname="text-[var(--color-text-secondary)]">Generate a recipe from your list of ingredients.</p>
+                <div className="pr-3 md:pr-5 pl-3 md:pl-5 pb-2">
+                    <h3 className="block !font-bold text-center md:text-left text-lg md:text-[1.17em] mt-[0.83em] mb-[0.1em] text-[var(--color-primary)]">Ready for a recipe?</h3>
+                    <p classname="text-sm md:text-base text-center md:text-left text-[var(--color-text-secondary)]">Generate a recipe from your list of ingredients.</p>
                 </div>
 
-                <button onClick={getRecipe} type="submit" className="btn-primary btn-hover">Get a recipe</button>
+                <button onClick={getRecipe} type="submit" className="w-full md:w-auto btn-primary btn-hover my-2 md:my-0">Get a recipe</button>
 
                 </div> : null}
 
